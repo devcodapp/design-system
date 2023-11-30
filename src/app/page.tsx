278ca9cfca94715d5ec6fe6d7d9e7450910feb7e
Anderson22-dev <andersonsilvasouza714@gmail.com>
2023-11-30 14:52:17 -0300

    Fix: deleted accordion component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
 'use client'
 
-import Accordion from "@/components/Accordion";
-import AccordionComp from "@/components/Accordion"
-import App from "@/components/accordionteste";
 import * as React from "react";
+import Header from "@/components/Header";
 
 export default function Home() {
 
     return (
         <div>
-            <App />
+            <Header title={"Clientes"} subtitle={"10 clientes"} />
             <h1>pagina inicial</h1>
-            <Accordion title="Titulo 1" content="Conteudo teste"></Accordion>
+
         </div>
     )
 }
