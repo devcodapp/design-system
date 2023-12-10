1c9c1cacee9ab4bd74645c1ff411dbf0be2cd600
Edaurdo <barrosoeduardo64@gmail.com>
2023-12-10 02:21:53 -0300

    feat: develop pixel perfect based on figma layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,12 @@ export default function Home() {
     return (
         <div>
             <Header 
-            title={"Clientes, administradores e outros"} 
-            subtitle={"10 clientes"} back={<a href="">BACK</a>} 
-            primaryActionBtn={<button className="bg-blue-600 h-min">Salvar</button>} 
-            secondaryActionBtn ={<button className="bg-white h-min">Edit</button>} 
-            terciaryActionBtn={<button className="bg-white h-min">Cancel</button>} />
+                title={"Clientes, administradores e outros"} 
+                subtitle={"10 clientes"} back={<a href="">BACK</a>} 
+                primaryActionBtn={<button className="bg-blue-600 h-min">Salvar</button>} 
+                secondaryActionBtn ={<button className="bg-white h-min">Edit</button>} 
+                tertiaryActionBtn={<button className="bg-white h-min">Cancel</button>} 
+            />
             <h1>pagina inicial</h1>
 
         </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,25 +6,34 @@ interface HeaderProps {
     back?: ReactNode;
     primaryActionBtn?: ReactNode;
     secondaryActionBtn?: ReactNode;
-    terciaryActionBtn?: ReactNode;  
+    tertiaryActionBtn?: ReactNode;  
 }
 
-export default function Header ({title,subtitle,back,primaryActionBtn,secondaryActionBtn,terciaryActionBtn}:HeaderProps) {
+export default function Header ({title,subtitle,back,primaryActionBtn,secondaryActionBtn, tertiaryActionBtn}:HeaderProps) {
     return (
-<div  className="px-9 py-8 font-system-font flex flex-col sm:flex-row justify-between bg-neutral-100 text-black">
-    <div className="flex gap-4 flex-wrap">
-        <div className="flex flex-col justify-center mb-2">
+<div  className="p-4 sm:px-9 sm:py-8 font-system-font gap-2 flex flex-wrap justify-between bg-neutral-100 text-black">
+    <div className="flex gap-3 gap-x-4 flex-wrap">
+        <div className="flex flex-col justify-center">
+            <div className="text-sm mb-1">
             {back? back: null}
-            <h1 className=" text-2xl font-bold text-neutral-800">{title}</h1>
+            </div>
+            <h1 className="text-lg sm:text-2xl font-bold text-neutral-800">{title}</h1>
             <h3 className="text-sm text-neutral-600">{subtitle}</h3>
         </div>
-        <div className="flex mb-2 items-center">
+        <div className="flex mb-2 items-center hidden sm:flex">
             {secondaryActionBtn? secondaryActionBtn: null}
         </div>
     </div>
-    <div className="flex gap-2 mb-2 items-center">
-        {terciaryActionBtn? terciaryActionBtn: null}
-        {primaryActionBtn? primaryActionBtn: null}
+    <div className="flex flex-wrap gap-2 items-center justify-normal">
+        <div className="flex items-center sm:hidden">
+            {secondaryActionBtn ? secondaryActionBtn : null}
+        </div>
+        <div>
+        {tertiaryActionBtn ? tertiaryActionBtn : null}
+        </div>
+        <div>
+        {primaryActionBtn ? primaryActionBtn : null}
+        </div>
     </div>
 </div>
     )
